3889d5e2031c48cdb0653fe54ce9ed7792671033
15370989688 <15370989688@163.com>
2016-12-18 23:19:51 +0800

    Initial Commit

---
+++ CRConfirmOrderServiceProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CRConfirmOrderServiceProtocol
+//
+//  Created by Charon on 16/12/18.
+//  Copyright © 2016年 Charon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
